b86164f89db129aad9af7c9082e942474b2f4fe2
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-01 18:28:29 -0700

    Add ability mark todo items as completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,28 @@ function App() {
     const newTodo = {
       id: Date.now(),
       title: todoTitle,
+      isCompleted: false,
     };
 
     setTodoList((previous) => [newTodo, ...previous]);
+  }
+
+  function completeTodo(id) {
+    setTodoList((previousTodoList) => {
+      return previousTodoList.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, isCompleted: true };
+        }
+        return todo;
+      });
+    });
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
