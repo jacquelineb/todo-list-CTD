1eee51551fcd0b9fddc6644e99229586f4e28d7e
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-09 18:11:06 -0700

    Allow user to edit todo items

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,22 @@ function App() {
     });
   }
 
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} />
     </div>
   );
 }
