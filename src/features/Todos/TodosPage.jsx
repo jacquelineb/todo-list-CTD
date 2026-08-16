92c728e06296f26be8129e08d6feb4d65d5fef40
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-15 19:44:55 -0700

    Add API integration with useEffect to fetch todolist after successful login

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList/TodoList.jsx';
 
-function TodosPage() {
+function TodosPage({ token }) {
   const [todoList, setTodoList] = useState([]);
+  const [error, setError] = useState('');
+  const [isTodoListLoading, setIsTodoListLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('fetching');
+    async function fetchTodos() {
+      setIsTodoListLoading(true);
+      try {
+        const params = new URLSearchParams({
+          limit: 100,
+        });
+        const response = await fetch(`/api/tasks?${params}`, {
+          headers: {
+            'X-CSRF-TOKEN': token,
+          },
+          credentials: 'include',
+        });
+
+        const result = await response.json();
+        if (response.status === 200) {
+          setTodoList(result.tasks);
+        } else if (response.status === 401) {
+          throw new Error(response.status);
+        } else {
+          throw new Error('Error fetching todos.');
+        }
+      } catch (error) {
+        setError(`Error: ${error.name} | ${error.message}`);
+      } finally {
+        setIsTodoListLoading(false);
+      }
+    }
+
+    if (token) {
+      fetchTodos();
+    }
+  }, [token]);
 
   function addTodo(todoTitle) {
     const newTodo = {
