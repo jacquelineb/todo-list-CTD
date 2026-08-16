aa0a623420a93431441f98cecddfd3ff067c3862
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-15 19:12:22 -0700

    Add authentication state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css';
+import { useState } from 'react';
 import Header from './shared/Header.jsx';
 import Logon from './features/Logon.jsx';
 import TodosPage from './features/Todos/TodosPage.jsx';
+import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
   return (
     <div>
-      <Header />
-      <Logon />
-      <TodosPage />
+      <Header token={token} onSetToken={setToken} onSetEmail={setEmail} />
+      {token ? (
+        <TodosPage token={token} />
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      )}
     </div>
   );
 }
