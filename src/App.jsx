9f005a1e7bda3f85e47b055aa3e694e13a1dca63
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-15 18:58:14 -0700

    Create Logon component with pessimistic UI update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './shared/Header.jsx';
+import Logon from './features/Logon.jsx';
 import TodosPage from './features/Todos/TodosPage.jsx';
 
 function App() {
   return (
     <div>
       <Header />
+      <Logon />
       <TodosPage />
     </div>
   );
---
+++ src/features/Logon.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+function Logon({ onSetEmail = () => {}, onSetToken = () => {} }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [authError, setAuthError] = useState('');
+  const [isLoggingOn, setIsLoggingOn] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoggingOn(true);
+    try {
+      const response = await fetch('/api/users/logon', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      if (response.status === 200 && data.name && data.csrfToken) {
+        onSetEmail(data.name);
+        onSetToken(data.csrfToken);
+      } else {
+        setAuthError(`Authentication failed: ${data?.message}`);
+      }
+    } catch (error) {
+      setAuthError(`Error: ${error.name} | ${error.message}`);
+    } finally {
+      setIsLoggingOn(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {authError ? <div>{authError}</div> : null}
+      <label htmlFor='user-email'>Email</label>
+      <input
+        type='text'
+        id='user-email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <label htmlFor='user-password'>Password</label>
+      <input
+        type='text'
+        id='user-password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type='submit' disabled={isLoggingOn}>
+        {isLoggingOn ? 'Logging In...' : 'Log On'}
+      </button>
+    </form>
+  );
+}
+
+export default Logon;
