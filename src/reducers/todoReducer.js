e4f01d4675cbb2f09a8bd5e3bd484b3900a4ec38
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-26 13:52:46 -0700

    Add reducer logic for FETCH_SUCCESS action

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -46,6 +46,15 @@ export function todoReducer(state, action) {
         error: '',
         filterError: '',
       };
+
+    case TODO_ACTIONS.FETCH_SUCCESS:
+      return {
+        ...state,
+        todoList: action.payload.todos,
+        isTodoListLoading: false,
+        error: '',
+        filterError: '',
+      };
     default:
       throw new Error(`Unknown action type: ${action.type}`);
   }
