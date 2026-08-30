dff3cc482218b79149a8b992eeab5dbdbce7a3a4
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-29 20:36:49 -0700

    Update initial sortDirection state

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -31,9 +31,9 @@ export const initialTodoState = {
   todoList: [],
   error: '',
   filterError: '',
-  isTodoListLoading: false,
+  isTodoListLoading: true,
   sortBy: 'createdAt',
-  sortDirection: 'desc',
+  sortDirection: 'asc',
   filterTerm: '',
   dataVersion: 0,
 };
@@ -172,7 +172,7 @@ export function todoReducer(state, action) {
         ...state,
         filterTerm: '',
         sortBy: 'createdAt',
-        sortDirection: 'desc',
+        sortDirection: 'asc',
         filterError: '',
       };
 
