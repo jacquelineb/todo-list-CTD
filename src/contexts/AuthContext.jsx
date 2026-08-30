3c75013cbdd265abe895f3e4c5093746bac92bd1
jacquelineb <jacquelinesotheremail@gmail.com>
2026-08-29 19:23:00 -0700

    Set up AuthContext

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,89 @@
+import { createContext, useContext, useState } from 'react';
+
+// Create the context
+const AuthContext = createContext();
+
+// Custom hook with error checking
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be uesd within an AuthProvider');
+  }
+  return context;
+}
+
+export function AuthProvider({ children }) {
+  // State for authentication
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
+
+  const login = async (userEmail, password) => {
+    try {
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: userEmail, password }),
+        credentials: 'include',
+      };
+
+      const res = await fetch('/api/users/logon', options);
+      const data = await res.json();
+
+      if (res.status === 200 && data.name && data.csrfToken) {
+        // Success: Update state
+        setEmail(data.name);
+        setToken(data.csrfToken);
+        return { success: true };
+      } else {
+        // Failure: Return error
+        return {
+          success: false,
+          error: `Authentication failed: ${data?.message}`,
+        };
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: 'Network error during login',
+      };
+    }
+  };
+
+  const logout = async (useEmail) => {
+    if (!token) {
+      setEmail('');
+      return;
+    }
+
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'X-CSRF-TOKEN': token,
+        },
+        credentials: 'include',
+      };
+
+      const res = await fetch('/api/users/logoff', options);
+    } catch (error) {
+      return {
+        success: false,
+        error: 'Network error during logout',
+      };
+    } finally {
+      setEmail('');
+      setToken('');
+    }
+  };
+
+  // Context value object
+  const value = {
+    email,
+    token,
+    isAuthenticated: !!token,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
