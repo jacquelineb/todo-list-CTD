a9cbc77dbd9f7555dd1c7867d951c66c2066d95f
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 19:20:53 -0700

    Update Logoff component to navigate to login page after successful logout

--- src/features/Logoff.jsx
+++ src/features/Logoff.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
 
 function Logoff() {
+  const [error, setError] = useState('');
+  const [isLoggingOff, setIsLoggingOff] = useState('false');
   const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogoff() {
+    setIsLoggingOff(true);
+    setError('');
+
+    const result = await logout();
+
+    if (result.success) {
+      navigate('/login');
+    } else {
+      setError(result.error);
+      setIsLoggingOff(false);
+    }
+  }
   return (
-    <button type='button' onClick={logout}>
+    <button type='button' onClick={handleLogoff}>
       Log Off
     </button>
   );
