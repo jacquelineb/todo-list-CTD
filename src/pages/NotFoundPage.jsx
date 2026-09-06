40071ff67335c0bc41e8d1ff44d78b8bf21d3a20
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 17:48:59 -0700

    Remove unnecessary semi-colon that was rendering on page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 function NotFoundPage() {
   return (
     <div>
-      <h2>Error 404 - Page not found.</h2>;
+      <h2>Error 404 - Page not found.</h2>
       <div>
         <Link to='/'>Return to homepage</Link>
       </div>
