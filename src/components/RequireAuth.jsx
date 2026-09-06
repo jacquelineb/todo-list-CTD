da319997c8b61163cfc0d9eaebe1b3ec4ab1a8c5
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 17:41:35 -0700

    Update return so that rendered children components aren't nested in a div

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -12,7 +12,7 @@ function RequireAuth({ children }) {
       navigate('/login', { state: { location } }); // not sure if state object in options is correct
     }
   }, [isAuthenticated, navigate, location]);
-  return <div>{!isAuthenticated ? 'Loading...' : { children }}</div>;
+  return <>{!isAuthenticated ? <div>Loading...</div> : { children }}</>;
 }
 
 export default RequireAuth;
