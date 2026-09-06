6da5297e46aa6721b3f57664499b86de3f5b7d54
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 19:13:26 -0700

    Render assignment implementation details as reminder that I'm not done with ProfilePage yet

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,12 +3,13 @@ import { useAuth } from '../contexts/AuthContext';
 function ProfilePage() {
   const { email } = useAuth();
 
-  // TODO: fetch todo statistics from API to display total/completed/active accounts. Will probably need token from useAuth
-  // Then display this information on page
-
   return (
     <div>
       <p>Welcome back, {email}</p>
+      <p>
+        TODO: fetch todo statistics from API to display total/completed/active accounts. Will
+        probably need token from useAuth Then display this information on page
+      </p>
     </div>
   );
 }
