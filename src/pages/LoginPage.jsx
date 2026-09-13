1914e4337ba97f3845aca99f388e57644ea54088
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-13 13:53:35 -0700

    Style login page
    - Move login error message below login form.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
+import styles from './LoginPage.module.css';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -39,29 +40,34 @@ function LoginPage() {
   }
 
   return (
-    <div>
-      {authError ? <div>{authError}</div> : null}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='user-email'>Email</label>
-        <input
-          type='text'
-          id='user-email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor='user-password'>Password</label>
-        <input
-          type='password'
-          id='user-password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+    <div className={styles.loginPage}>
+      <h2>Log In</h2>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='user-email'>Email</label>
+          <input
+            type='text'
+            id='user-email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='user-password'>Password</label>
+          <input
+            type='password'
+            id='user-password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
         <button type='submit' disabled={isLoggingOn}>
           {isLoggingOn ? 'Logging in...' : 'Log On'}
         </button>
       </form>
+      {authError ? <div className={styles.loginError}>{authError}</div> : null}
     </div>
   );
 }
