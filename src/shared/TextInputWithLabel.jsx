d4153037f37bffeb4973c4234ecc40252ecd3b1e
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-13 16:05:33 -0700

    Add placeholder prop to TextInputWithLabel

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,8 +1,15 @@
-function TextInputWithLabel({ elementId, labelText, onChange, ref, value }) {
+function TextInputWithLabel({ elementId, labelText, onChange, ref, value, placeholder }) {
   return (
     <>
       <label htmlFor={elementId}>{labelText}</label>
-      <input type='text' id={elementId} ref={ref} value={value} onChange={onChange} />
+      <input
+        type='text'
+        id={elementId}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </>
   );
 }
